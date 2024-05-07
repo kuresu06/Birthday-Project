a8c2201e5dc5f405089ac9f2740c872aa9aa793f
Liutauras Stepanenko <liutauras.stepanenko@gmail.com>
2024-05-07 20:47:07 +0300

    Finish webscraping and storing data method for holidays (Other website).

--- project.py
+++ project.py
@@ -1,23 +1,33 @@
-from datetime import datetime
+import datetime
 import csv
-import re
+from re import match
 import sys
-import requests
+from requests import get
 from bs4 import BeautifulSoup
+
+today = datetime.datetime.now()
+months = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
+
 
 def main():
     humans = read_human("./Data/birthdays.csv")
     add_human('a', 'b', '2004-07-22', humans)
+    print('-----------------Bdays:')
     for human in humans:
         #print(human)
-        print(human.get_next_birthday())
+        print(human.get_next_time())
+
+    print('-----------------Holidays:')
+    hol = get_holidays()
+    for h in hol:
+        print(h.get_next_time())
 
 class Human():
     def __init__(self, first, last, birthday):
         self.first = first
         self.last = last
         try:
-            self.birthday = datetime.fromisoformat(birthday)
+            self.birthday = datetime.datetime.fromisoformat(birthday)
         except ValueError:
             sys.exit("Incorrect date")
 
@@ -27,8 +37,7 @@ class Human():
     def get_born(self):
         return self.birthdate
 
-    def get_next_birthday(self):
-        today = datetime.today()
+    def get_next_time(self):
         b = self.birthday.replace(year = today.year)
         time = b - today
         if(time.days<0):
@@ -51,38 +60,36 @@ def write_human(file):
     return
 
 class Holiday():
-    def __init__(self, date, en_name, lt_name, remarks):
-        self.date = date
-        self.en_name = en_name
-        self.lt_name = lt_name
-        self.remarks = remarks
+    def __init__(self, name, date):
+        self.name = name
+        (m , d) = date.split(",")[0].split(" ")
+        if(m in months.keys() and d.isdigit()):
+            self.date = datetime.datetime(today.year, months[m], int(d))
+    
+    def get_next_time(self):
+        time = self.date - today
+        if(time.days<0):
+            time = self.date.replace(year = today.year+1) - today
+        return time
+    
+    def __str__(self):
+        return f"{self.name} {self.date}"
 
-def read_holidays():
-    url = 'https://en.wikipedia.org/wiki/Public_holidays_in_Lithuania'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    table = soup.find('table', class_="wikitable")
+def get_holidays():
+    url = f'https://www.wincalendar.com/Calendar-Lithuania/{today.year}'
+    r = get(url) # sends get request
+    soup = BeautifulSoup(r.content, 'html.parser') #parses gotten html
+    table = soup.find('table', class_="thlinks") #finds the table where the data is stored
 
-    headers = table.find_all('th')
-    header = []
-    for h in headers:
-        if t:= h.get_text(strip=True):
-            header.append(t)
-
-    data=[]
     trs = table.find_all('tr')
-    for field in trs:
-        new = []
-        tds = field.find_all('td')
-        for t in tds:
-            if t.get_text(strip=True):
-                new.append(t.get_text(strip=True))
-        if(len(new)!=0):
-            d = {}
-            for i in range(len(new)):
-                d.update({header[i]:new[i]})
-            data.append(d)
 
+    holidays = []
+    for tr in trs:
+        tds = tr.find_all('td')
+        a = tr.find('a').get_text()
+        if(t:=match(r".*Date (.*) in a Calendar.*", tds[3].a['title']).group(1)):
+            holidays.append(Holiday(a, t))
+    return holidays
 
 if __name__ == "__main__":
     main()
